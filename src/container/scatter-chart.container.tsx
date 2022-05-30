bbf23623f91696ef61ea1bd9d09bfd9a97a4b215
Dongyoung Lim <limdongyoung0@gmail.com>
2022-05-30 16:23:17 +0900

    Implement basic ScatterChartContainer

--- src/container/scatter-chart.container.tsx
+++ src/container/scatter-chart.container.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { GradientSteelPurple } from "@visx/gradient";
+import { Group } from "@visx/group";
+import { scaleLinear } from "@visx/scale";
+import { Circle } from "@visx/shape";
+import React, { useMemo, useRef } from "react";
+import { getXFromPointsRange, getYFromPointsRange } from "../lib/data-util";
+import { genMockPoints } from "../lib/mock-data-util";
 
-function ScatterChartContainer() {
-  return <div>Scatter Chart Container</div>;
+const pointsMock = genMockPoints(600);
+
+export type ScatterChartContainerProps = {
+  width: number;
+  height: number;
+};
+
+function ScatterChartContainer({ width, height }: ScatterChartContainerProps) {
+  const svgRef = useRef<SVGSVGElement>(null);
+  const xScale = useMemo(
+    () =>
+      scaleLinear<number>({
+        domain: [1.3, 2.2],
+        range: [0, width],
+        clamp: true,
+      }),
+    [width]
+  );
+  const yScale = useMemo(
+    () =>
+      scaleLinear<number>({
+        domain: [0.75, 1.6],
+        range: [height, 0],
+        clamp: true,
+      }),
+    [height]
+  );
+
+  return (
+    <div>
+      <svg width={width} height={height} ref={svgRef}>
+        <GradientSteelPurple id="dots-purple" />
+        <rect width={width} height={height} rx={14} fill="url(#dots-purple)" />
+        <Group pointerEvents="none">
+          {pointsMock.map((point, i) => (
+            <Circle
+              key={`point-${point[0]}-${i}`}
+              className="dot"
+              cx={xScale(getXFromPointsRange(point))}
+              cy={yScale(getYFromPointsRange(point))}
+              r={5}
+              fill="white"
+            />
+          ))}
+        </Group>
+      </svg>
+    </div>
+  );
 }
 
 export default ScatterChartContainer;
