0747473b3b185297868b5d2a94eeee85cf87a5c9
Dongyoung Lim <limdongyoung0@gmail.com>
2022-05-30 16:34:27 +0900

    Apply react router dom for various example

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ScatterChartContainer from "./container/scatter-chart.container";
+import { Link } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <ScatterChartContainer width={600} height={400} />
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/scatter-chart-container">ScatterChartContainer</Link>
+      </nav>
     </div>
   );
 }
