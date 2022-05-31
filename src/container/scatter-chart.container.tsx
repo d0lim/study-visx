32362637089118a415bae624dba6166c4813103c
D0Lim <limdongyoung0@gmail.com>
2022-05-31 11:09:01 +0900

    Feature scatter chart tooltip (#3)
    * Add tooltip with mouseover event to circle
    
    * Fix position of tooltip
    
    * Applied bound detective tooltip

--- src/container/scatter-chart.container.tsx
+++ src/container/scatter-chart.container.tsx
@@ -1,9 +1,10 @@
 import { AxisBottom, AxisLeft } from "@visx/axis";
-import { GradientSteelPurple } from "@visx/gradient";
+import localPoint from "@visx/event/lib/localPointGeneric";
 import { Group } from "@visx/group";
 import { scaleLinear } from "@visx/scale";
 import { Circle } from "@visx/shape";
-import React, { useMemo, useRef } from "react";
+import { TooltipWithBounds, useTooltip } from "@visx/tooltip";
+import React, { useCallback, useMemo, useRef } from "react";
 import { getXFromPointsRange, getYFromPointsRange } from "../lib/data-util";
 import { genMockPoints } from "../lib/mock-data-util";
 
@@ -42,15 +43,55 @@ function ScatterChartContainer({ width, height }: ScatterChartContainerProps) {
   const xScale = useMemo(() => xAxisScale, [width]);
   const yScale = useMemo(() => yAxisScale, [height]);
 
+  let tooltipTimeout: number;
+
+  const {
+    showTooltip,
+    hideTooltip,
+    tooltipOpen,
+    tooltipData,
+    tooltipLeft = 0,
+    tooltipTop = 0,
+  } = useTooltip<number[]>({
+    // initial tooltip state
+    tooltipOpen: true,
+    tooltipLeft: width / 3,
+    tooltipTop: height / 3,
+    tooltipData: [0, 0, 0],
+  });
+
+  const handleMouseOver = useCallback(
+    (event: React.MouseEvent | React.TouchEvent) => {
+      if (tooltipTimeout) clearTimeout(tooltipTimeout);
+      if (!svgRef.current) return;
+
+      const currentPoint = localPoint(svgRef.current, event);
+
+      console.log(currentPoint);
+
+      if (!currentPoint) return;
+      else {
+        showTooltip({
+          tooltipLeft: currentPoint.x,
+          tooltipTop: currentPoint.y,
+          tooltipData: [currentPoint.x, currentPoint.y, 0],
+        });
+      }
+    },
+    [xScale, yScale, showTooltip]
+  );
+
+  const handleMouseLeave = useCallback(() => {
+    tooltipTimeout = window.setTimeout(() => {
+      hideTooltip();
+    }, 300);
+  }, [hideTooltip]);
+
   return (
     <div style={{ margin: "50px" }}>
       <svg width={width} height={height} ref={svgRef}>
         <rect width={width} height={height} rx={10} fill="white" />
-        <Group
-          pointerEvents="none"
-          left={defaultMargin.left}
-          top={defaultMargin.top}
-        >
+        <Group left={defaultMargin.left} top={defaultMargin.top}>
           <AxisBottom scale={xAxisScale} top={yMax} />
           <AxisLeft scale={yAxisScale} />
           {pointsMock.map((point, i) => (
@@ -61,10 +102,24 @@ function ScatterChartContainer({ width, height }: ScatterChartContainerProps) {
               cy={yScale(getYFromPointsRange(point))}
               r={5} // 여기가 점 크기이다
               fill="purple"
+              onMouseMove={handleMouseOver}
+              onMouseLeave={handleMouseLeave}
+              onTouchMove={handleMouseOver}
+              onTouchEnd={handleMouseLeave}
             />
           ))}
         </Group>
       </svg>
+      {tooltipOpen && tooltipData && tooltipLeft != null && tooltipTop != null && (
+        <TooltipWithBounds left={tooltipLeft + 10} top={tooltipTop + 10}>
+          <div style={{ width: "200px" }}>
+            <strong>x:</strong> {tooltipData[0]}
+          </div>
+          <div>
+            <strong>y:</strong> {tooltipData[1]}
+          </div>
+        </TooltipWithBounds>
+      )}
     </div>
   );
 }
